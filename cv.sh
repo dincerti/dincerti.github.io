9e223c375887e805cb9a1402d8e2ebbe237414dd
Devin.Incerti <devin.incerti@OAK-MB-DINCERTI.local>
2017-02-20 19:53:42 -0800

    New job title

--- cv.sh
+++ cv.sh
@@ -4,7 +4,7 @@
 /Applications/RStudio.app/Contents/MacOS/pandoc/pandoc cv-pandoc.md --template latex-cv-template.tex \
 --variable author="Devin Incerti" --variable title="CV" --variable linkcolor="blue" \
 --variable fontfamily="mathpazo" --variable fontfamilyoptions="sc, osf" --variable fontsize="11pt" \
---variable geometry="margin=1in" --variable jobtitle="Associate Research Economist" \
+--variable geometry="margin=1in" --variable jobtitle="Research Economist" \
 --variable email="devin.incerti@PHeconomics.com" --variable fontawesome="yes" \
 --variable address="555 12th Street, Suite 250, Oakland, CA 94607" --variable urlcolor="blue" \
 --variable web="devinincerti.com" \
